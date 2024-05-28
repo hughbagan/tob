c226a7e1197514b012193b4daed28f805ca3a58b
hughbagan <25750667+hughbagan@users.noreply.github.com>
2024-05-27 22:21:22 -0600

    Added jumping, landing, WIP destroying leaves

--- scenes/Level.gd
+++ scenes/Level.gd
@@ -5,23 +5,26 @@ const PLAYER_SCENE:PackedScene = preload("res://scenes/Player.tscn")
 onready var generator = $WFCGenerator
 onready var target_tilemap:TileMap = $WFCGenerator/Target
 onready var envelope_tilemap:TileMap = $WFCGenerator/Target/Envelope
-onready var camera:Camera2D = $Camera2D
+onready var camera:Camera2D = $WFCGenerator/Target/Camera2D
 var player:Player
 
-func _ready():
-	pass # Replace with function body.
 
 func _on_WFCGenerator_OnDone():
-	# encase the level in an invisible wall
-	envelope_tilemap.position = -target_tilemap.cell_size
 	var width:int = generator.H+1
 	var height:int = generator.V+1
-	for y in range(height):
-		for x in range(width):
-			if (x==0 or y==0) or (x==width-1 or y==height-1):
-				envelope_tilemap.set_cell(x, y, LEVEL_WALL_TILE_ID)
-	camera.position = envelope_tilemap.position
+
+	# encase the level in an invisible wall
+	# envelope_tilemap.position = -target_tilemap.cell_size
+	# for y in range(height):
+	# 	for x in range(width):
+	# 		if (x==0 or y==0) or (x==width-1 or y==height-1):
+	# 			envelope_tilemap.set_cell(x, y, LEVEL_WALL_TILE_ID)
+
+	# TODO: spawn entities to replace tiles
+
 	# Deploy the player
 	player = PLAYER_SCENE.instance()
-	player.global_position = envelope_tilemap.map_to_world(Vector2(width/2, height/2))-target_tilemap.cell_size/2
+	player.tilemap = target_tilemap
+	player.global_position = target_tilemap.map_to_world(Vector2(width/2, height/2))-target_tilemap.cell_size/2
 	add_child(player)
+	camera.position = target_tilemap.get_used_rect().size*target_tilemap.cell_size*0.5
--- scenes/Player.gd
+++ scenes/Player.gd
@@ -1,16 +1,46 @@
 class_name Player extends KinematicBody2D
 
-var speed:float = 100.0
+onready var sprite:Sprite = $Sprite
+onready var jump_timer:Timer = $JumpTimer
+var speed:float = 75.0
 var velocity:Vector2 = Vector2()
+var jumping:bool = false
+var tilemap:TileMap
+var current_tile_coords:Vector2
+var current_tile:int
+
 
 func _physics_process(_delta):
 	velocity = Vector2()
-	if Input.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
-	if Input.is_action_pressed("ui_left"):
+	if Input.is_action_pressed("move_left"):
 		velocity.x -= 1
-	if Input.is_action_pressed("ui_down"):
+	if Input.is_action_pressed("move_down"):
 		velocity.y += 1
-	if Input.is_action_pressed("ui_up"):
+	if Input.is_action_pressed("move_up"):
 		velocity.y -= 1
 	velocity = move_and_slide(velocity.normalized() * speed)
+
+	if Input.is_action_pressed("jump") and not jumping:
+		jumping = true
+		jump_timer.start()
+		var jump_tween = get_tree().create_tween()
+		jump_tween.tween_property(self, "scale", Vector2(1.5, 1.5), jump_timer.wait_time*0.5).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_QUART)
+		jump_tween.chain().tween_property(self, "scale", Vector2(1.0, 1.0), jump_timer.wait_time*0.5).set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_QUART)
+
+	current_tile_coords = tilemap.world_to_map(tilemap.to_local(global_position))
+	current_tile = tilemap.get_cellv(current_tile_coords)
+	if not jumping:
+		if current_tile >= 5 and current_tile <= 7:
+			get_tree().create_timer(1.0, false).connect("timeout", self, "_on_leaf_destroy", [current_tile_coords])
+		elif current_tile <= 0:
+			get_tree().reload_current_scene()
+
+
+func _on_JumpTimer_timeout():
+	jumping = false
+
+
+func _on_leaf_destroy(tile_coords:Vector2):
+	tilemap.set_cellv(tile_coords, 0)
