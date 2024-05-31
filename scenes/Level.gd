f7aca6cc53cbc754a7f6d8ced32fc0f10f0d68bd
hughbagan <25750667+hughbagan@users.noreply.github.com>
2024-05-31 16:39:55 -0600

    Add shadow, Enemy, and Player bouncing on Enemy
    set_cell not doing anything for some reason?

--- scenes/Level.gd
+++ scenes/Level.gd
@@ -1,12 +1,13 @@
 class_name Level extends Node2D
 
 const LEVEL_WALL_TILE_ID:int = 24
+const LEVEL_ENEMY_TILE_ID:int = 22
 const PLAYER_SCENE:PackedScene = preload("res://scenes/Player.tscn")
-onready var generator = $WFCGenerator
+const ENEMY_SCENE:PackedScene = preload("res://scenes/Enemy.tscn")
+onready var generator:Node2D = $WFCGenerator
 onready var target_tilemap:TileMap = $WFCGenerator/Target
 onready var envelope_tilemap:TileMap = $WFCGenerator/Target/Envelope
 onready var camera:Camera2D = $WFCGenerator/Target/Camera2D
-var player:Player
 
 
 func _on_WFCGenerator_OnDone():
@@ -20,11 +21,34 @@ func _on_WFCGenerator_OnDone():
 	# 		if (x==0 or y==0) or (x==width-1 or y==height-1):
 	# 			envelope_tilemap.set_cell(x, y, LEVEL_WALL_TILE_ID)
 
-	# TODO: spawn entities to replace tiles
+	# Spawn entities to replace tiles
+	for y in range(height):
+		for x in range(width):
+			print(x, " ", y, " ", target_tilemap.get_cell(x,y))
+			if target_tilemap.get_cell(x, y) == LEVEL_ENEMY_TILE_ID:
+				var enemy = ENEMY_SCENE.instance()
+				enemy.global_position = place_centered_tile(Vector2(x,y))
+				add_child(enemy)
 
 	# Deploy the player
-	player = PLAYER_SCENE.instance()
+	var player = PLAYER_SCENE.instance()
 	player.tilemap = target_tilemap
-	player.global_position = target_tilemap.map_to_world(Vector2(width/2, height/2))-target_tilemap.cell_size/2
+	player.level = self
+	var testpos = Vector2(width/2, height/2)
+	while target_tilemap.get_cellv(testpos) == LEVEL_WALL_TILE_ID:
+		var flip = randf()
+		if flip <= 0.25:
+			testpos.x += 1
+		elif flip <= 0.5:
+			testpos.x -= 1
+		elif flip <= 0.75:
+			testpos.y += 1
+		elif flip <= 1.0:
+			testpos.y -= 1
+	player.global_position = place_centered_tile(testpos)
 	add_child(player)
 	camera.position = target_tilemap.get_used_rect().size*target_tilemap.cell_size*0.5
+
+
+func place_centered_tile(pos:Vector2) -> Vector2:
+	return target_tilemap.map_to_world(pos)+target_tilemap.cell_size/2
--- scenes/Player.gd
+++ scenes/Player.gd
@@ -1,13 +1,17 @@
 class_name Player extends KinematicBody2D
 
 onready var sprite:Sprite = $Sprite
+onready var spr_scale:Vector2 = sprite.scale
+onready var col:CollisionShape2D = $CollisionShape2D
+onready var jump_area:Area2D = $JumpArea
 onready var jump_timer:Timer = $JumpTimer
-var speed:float = 75.0
-var velocity:Vector2 = Vector2()
-var jumping:bool = false
 var tilemap:TileMap
 var current_tile_coords:Vector2
 var current_tile:int
+var level
+var speed:float = 75.0
+var velocity:Vector2 = Vector2()
+var jumping:bool = false
 
 
 func _physics_process(_delta):
@@ -22,24 +26,34 @@ func _physics_process(_delta):
 		velocity.y -= 1
 	velocity = move_and_slide(velocity.normalized() * speed)
 
-	if Input.is_action_pressed("jump") and not jumping:
-		jumping = true
-		jump_timer.start()
-		var jump_tween = get_tree().create_tween()
-		jump_tween.tween_property(self, "scale", Vector2(1.5, 1.5), jump_timer.wait_time*0.5).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_QUART)
-		jump_tween.chain().tween_property(self, "scale", Vector2(1.0, 1.0), jump_timer.wait_time*0.5).set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_QUART)
-
 	current_tile_coords = tilemap.world_to_map(tilemap.to_local(global_position))
 	current_tile = tilemap.get_cellv(current_tile_coords)
-	if not jumping:
-		if current_tile >= 5 and current_tile <= 7:
-			get_tree().create_timer(1.0, false).connect("timeout", self, "_on_leaf_destroy", [current_tile_coords])
-		elif current_tile <= 0:
-			get_tree().reload_current_scene()
+
+	if Input.is_action_pressed("jump") and not jumping:
+		jump()
+	# if not jumping:
+	# 	if current_tile >= 5 and current_tile <= 7:
+	# 		get_tree().create_timer(1.0, false).connect("timeout", self, "_on_leaf_destroy", [current_tile_coords])
+	# 	elif current_tile <= 0:
+	# 		get_tree().reload_current_scene()
+
+
+func jump():
+	jumping = true
+	jump_timer.start()
+	var jump_tween = get_tree().create_tween()
+	jump_tween.tween_property(sprite, "scale", spr_scale*1.5, jump_timer.wait_time*0.5).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_QUART)
+	jump_tween.chain().tween_property(sprite, "scale", spr_scale, jump_timer.wait_time*0.5).set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_QUART)
+	col.disabled = true
 
 
 func _on_JumpTimer_timeout():
+	# Landing
 	jumping = false
+	col.disabled = false
+	for body in jump_area.get_overlapping_bodies():
+		body.queue_free()
+		jump()
 
 
 func _on_leaf_destroy(tile_coords:Vector2):
