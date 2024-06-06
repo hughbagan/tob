dab31e4a91b4423171ff47016aea03ce3bfed6d2
hughbagan <25750667+hughbagan@users.noreply.github.com>
2024-06-05 23:49:59 -0600

    Enemy line of sight to player boilerplate
    Pathfinding to come

--- scenes/Enemy.gd
+++ scenes/Enemy.gd
@@ -1,10 +1,37 @@
 class_name Enemy extends KinematicBody2D
 
+enum STATES {SENTRY, ALERT}
+var state = STATES.SENTRY
+var level
+var tilemap
+var player
+onready var sight_timer:Timer = $SightTimer
+onready var raycast:RayCast2D = $RayCast2D
+var sight_distance:int = 5 # in tiles
+var current_tile_coords:Vector2
+
 
 func _process(_delta):
 	if Input.is_action_just_pressed("bloodvision"):
-		$Sprite.hide()
 		$SpriteShaded.show()
 	if Input.is_action_just_released("bloodvision"):
-		$Sprite.show()
 		$SpriteShaded.hide()
+
+	current_tile_coords = tilemap.world_to_map(tilemap.to_local(global_position))
+	match state:
+		STATES.SENTRY:
+			if current_tile_coords.distance_to(player.current_tile_coords) <= 2 \
+			and sight_timer.is_stopped():
+				sight_timer.start()
+		STATES.ALERT:
+			# chase the player
+			pass
+
+
+func _on_SightTimer_timeout():
+	raycast.cast_to = raycast.to_local(player.global_position)
+	raycast.force_raycast_update()
+	var collider = raycast.get_collider()
+	if collider == player:
+		state = STATES.ALERT
+		print(self, " spotted player!")
