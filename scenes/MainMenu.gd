1085deeb511d47039c84b009362f320768b36f09
hughbagan <25750667+hughbagan@users.noreply.github.com>
2024-06-07 02:22:47 -0600

    add credits screen to MainMenu

--- scenes/MainMenu.gd
+++ scenes/MainMenu.gd
@@ -1,10 +1,22 @@
 extends Control
 
 
+func _ready() -> void:
+	$Credits.hide()
+
+
 func _on_PlayButton_pressed() -> void:
 	$StepsSound.play()
 	$RedRect.show()
 	var tween = get_tree().create_tween()
-	tween.tween_property($RedRect, "color:a", 1.0, 1.0)
+	tween.tween_property($RedRect, "color:a", 1.0, 1.5)
 	yield(tween, "finished")
 	get_tree().change_scene("res://scenes/Tutorial.tscn")
+
+
+func _on_InfoButton_pressed():
+	$Credits.show()
+
+
+func _on_BackButton_pressed():
+	$Credits.hide()
