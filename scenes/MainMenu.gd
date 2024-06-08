f7acd80ed3896da9c0931916524fbe437f36df4a
Aiden CC <a.collinscruickshank@gmail.com>
2024-06-08 15:13:35 -0600

    Added eases to music in MainMenu

--- scenes/MainMenu.gd
+++ scenes/MainMenu.gd
@@ -3,7 +3,7 @@ extends Control
 
 func _ready() -> void:
 	$Credits.hide()
-	MusicMan.MainMenu(true, 1)
+	MusicMan.MainMenu(true, 1, .75)
 
 
 func _on_PlayButton_pressed() -> void:
@@ -19,11 +19,11 @@ func _on_PlayButton_pressed() -> void:
 
 func _on_InfoButton_pressed():
 	$Credits.show()
-	MusicMan.MainMenu(false, 2)
-	MusicMan.Credits(true, 1)
+	MusicMan.MainMenu(false, 2, 0)
+	MusicMan.Credits(true, 1, 1)
 
 
 func _on_BackButton_pressed():
 	$Credits.hide()
-	MusicMan.Credits(false, 1)
-	MusicMan.MainMenu(true, 1)
+	MusicMan.Credits(false, 1, 0)
+	MusicMan.MainMenu(true, 1, 1)
